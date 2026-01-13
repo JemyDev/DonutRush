8c7b717de31f501cb199da20de1f7aa70bfec08c
JemyDev <jeremy.delhaie@gmail.com>
2026-01-13 02:43:27 +0100

    Update DoorController and Door Prefab

--- Assets/Components/Door/Scripts/DoorController.cs
+++ Assets/Components/Door/Scripts/DoorController.cs
@@ -1,7 +1,38 @@
-using System;
 using UnityEngine;
 
+/// <summary>
+/// Door controller that updates Door prefab based on distributed ingredient
+/// </summary>
 public class DoorController : MonoBehaviour
 {
+    [SerializeField] private IngredientScriptableObject _ingredient;
+    [SerializeField] private MeshRenderer[] _wallsMeshRenderer;
+
+    private void Awake()
+    {
+        GameEventSystem.OnIngredientDistributed += AttachIngredient;
+    }
+
+    private void OnDestroy()
+    {
+        GameEventSystem.OnIngredientDistributed -= AttachIngredient;
+    }
     
+    private void AttachIngredient(IngredientScriptableObject ingredient)
+    {
+        if (_ingredient) return;
+        
+        _ingredient = ingredient;
+        UpdateWallsMaterial();
+    }
+    
+    private void UpdateWallsMaterial()
+    {
+        if (!_ingredient) return;
+        
+        foreach (var wall in _wallsMeshRenderer)
+        {
+            wall.material = _ingredient.ingredientMaterial;
+        }
+    }
 }
