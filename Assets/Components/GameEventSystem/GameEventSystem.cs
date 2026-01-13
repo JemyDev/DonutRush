40ae951d385a6728c8c8fd22c3000805195349c7
JemyDev <jeremy.delhaie@gmail.com>
2026-01-13 02:40:20 +0100

    Update GameEventSystem
    - Add OnDoorInstantiated
    - Add OnIngredientDistributed

--- Assets/Components/GameEventSystem/GameEventSystem.cs
+++ Assets/Components/GameEventSystem/GameEventSystem.cs
@@ -7,4 +7,6 @@ public static class GameEventSystem
     public static Action<string> OnIngredientCollected;
     public static Action<Dictionary<string, OrderLine>> OnOrderCreated;
     public static Action OnOrderCompleted;
+    public static Action OnDoorInstantiated;
+    public static Action<IngredientScriptableObject> OnIngredientDistributed;
 }
