db9553d7467092ece7c4c30933ded6f5e0013153
JemyDev <jeremy.delhaie@gmail.com>
2026-01-26 22:46:30 +0100

    Add ScriptableObjectDatabase

--- Assets/Components/ScriptableObjectDatabase/ScriptableObjectDatabase.cs
+++ Assets/Components/ScriptableObjectDatabase/ScriptableObjectDatabase.cs
@@ -1,32 +1,79 @@
+using System;
 using System.Collections.Generic;
+using Components.Data;
 using UnityEngine;
 
-namespace Components.ScriptableObjectDatabase
+namespace Components.SODatabase
 {
-    public static class ScriptableObjectDatabase<T> where T : ScriptableObject
+    public static class ScriptableObjectDatabase
     {
-        private static readonly Dictionary<string, T> DATABASE = new();
+        private static readonly Dictionary<Type, Dictionary<string, ScriptableObject>> DATABASE = new();
         
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
         private static void Initialize()
         {
             DATABASE.Clear();
-            var scriptableObjects = Resources.LoadAll<T>("Data/" + typeof(T).Name);
-
-            foreach (var scriptableObject in scriptableObjects)
-            {
-                DATABASE.Add(scriptableObject.name, scriptableObject);
-            }
+            
+            Register<LevelParametersData>();
+            Register<IngredientData>();
         }
 
-        public static T GetByName(string name)
+        private static void Register<T>() where T : ScriptableObject
         {
-            if (DATABASE.TryGetValue(name, out var result))
+            var type = typeof(T);
+
+            if (DATABASE.ContainsKey(type))
             {
-                return result;
+                Debug.Log("Type already registered: " + type.Name);
+                return;
+            }
+
+            DATABASE[type] = new Dictionary<string, ScriptableObject>();
+            
+            var scriptableObjects = Resources.LoadAll<T>("");
+            foreach (var scriptableObject in scriptableObjects)
+            {
+                DATABASE[type][scriptableObject.name] = scriptableObject;
             }
             
-            Debug.LogWarning($"Scriptable object {name} not found");
+            Debug.Log($"Registered {scriptableObjects.Length} scriptable objects of type {type.Name}");
+        }
+
+        public static T Get<T>(string name) where T : ScriptableObject
+        {
+            var type = typeof(T);
+
+            if (DATABASE.TryGetValue(type, out var dataTypes))
+            {
+                if (dataTypes.TryGetValue(name, out var scriptableObject))
+                {
+                    return scriptableObject as T;
+                }
+            }
+            
+            Debug.LogError($"ScriptableObject of type {type.Name} with name {name} not found.");
             return null;
+        }
+        
+        public static T[] GetAll<T>() where T : ScriptableObject
+        {
+            var type = typeof(T);
+
+            if (DATABASE.TryGetValue(type, out var dataTypes))
+            {
+                var list = new List<T>();
+                foreach (var scriptableObject in dataTypes.Values)
+                {
+                    if (scriptableObject is T typedObject)
+                    {
+                        list.Add(typedObject);
+                    }
+                }
+                return list.ToArray();
+            }
+            
+            Debug.LogError($"No ScriptableObjects of type {type.Name} found.");
+            return Array.Empty<T>();
         }
     }
 }
